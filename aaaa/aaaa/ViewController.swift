59f3aa6e1c74699e692b2265a250203a6040d2ce
古賀貴伍 <jpx02428@linecorp.com>
2022-03-04 10:34:42 +0900

    確認用

---
+++ aaaa/aaaa/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  aaaa
+//
+//  Created by 古賀貴伍社用 on 2022/03/02.
+//
+
+import UIKit
+import GoogleInteractiveMediaAds
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ aaaa/aaaaUITests/aaaaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  aaaaUITestsLaunchTests.swift
+//  aaaaUITests
+//
+//  Created by 古賀貴伍社用 on 2022/03/02.
+//
+
+import XCTest
+
+class aaaaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
